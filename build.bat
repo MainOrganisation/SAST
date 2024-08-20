60d10d1710e5d79751902e0103644568150036d3
Daniel Pereira <81535464+danielrspereira@users.noreply.github.com>
2024-08-20 10:10:26 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,20 +1,20 @@
 @echo off
 
-REM Set the path to MSBuild. This is typically in your Visual Studio installation directory.
-REM Modify the following line with the correct path for your environment.
+echo Set the path to MSBuild. This is typically in your Visual Studio installation directory.
+echo Modify the following line with the correct path for your environment.
 
 set MSBUILD_PATH="C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
 
-REM Set the path to the .sln or .vcxproj file
+echo Set the path to the .sln or .vcxproj file
 set PROJECT_PATH="C:\actions-runner\_work\_tool\SAST.sln"
 
-REM Set the build configuration (Debug or Release)
+echo Set the build configuration (Debug or Release)
 set CONFIGURATION=Release
 
-REM Set the target platform (e.g., x86, x64)
+echo Set the target platform (e.g., x86, x64)
 set PLATFORM=x64
 
-REM Build the project using MSBuild
+echo Build the project using MSBuild
 %MSBUILD_PATH% %PROJECT_PATH% /p:Configuration=%CONFIGURATION% /p:Platform=%PLATFORM%
 
 REM Pause the script to see the result (optional)
